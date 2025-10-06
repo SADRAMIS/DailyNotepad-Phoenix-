1396a839f7554519263c1545acaf802b2ebd4fa3
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-10-06 16:59:37 +0300

    Init commit

---
+++ src/main/java/com/example/dailynotes/DailynotepadPhoenixApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailynotepadPhoenixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailynotepadPhoenixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dailynotes/DailynotepadPhoenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailynotepadPhoenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
