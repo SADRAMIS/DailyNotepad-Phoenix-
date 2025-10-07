fd0d8cacdf06b191f89bdabc9aca3ca0dee40324
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-10-07 16:15:56 +0300

    add NoteService

---
+++ src/main/java/com/example/dailynotes/service/NoteService.java
@@ -0,0 +1,22 @@
+package com.example.dailynotes.service;
+
+import com.example.dailynotes.entity.Note;
+import com.example.dailynotes.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteService {
+
+    private NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository){
+        this.noteRepository = noteRepository;
+    }
+
+    @Autowired
+    public void createNote(String title, String content){
+        Note note = new Note(title,content);
+        noteRepository.save(note);
+    }
+}
