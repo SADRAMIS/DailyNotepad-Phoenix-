2f1b6810c622e7cd203ce1eff87bbbaac5ed3d6f
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-10-28 19:31:25 +0300

    add MonthlyTask

---
+++ src/main/java/com/example/dailynotes/entity/MonthlyTask.java
@@ -0,0 +1,23 @@
+package com.example.dailynotes.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Map;
+
+@Entity
+public class MonthlyTask {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private Task task;
+
+    @ManyToOne
+    private MonthlyPlan monthlyPlan;
+
+    //храним статусы завершения для каждого дня месяца
+    @ElementCollection
+    private Map<Integer,Boolean> statusPerDay;
+
+}
