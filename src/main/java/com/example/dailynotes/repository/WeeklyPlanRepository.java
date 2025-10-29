96d7528c930dc7a341390782fd3134073d6003c7
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-10-29 16:08:24 +0300

    add WeeklyPlanRepository

---
+++ src/main/java/com/example/dailynotes/repository/WeeklyPlanRepository.java
@@ -0,0 +1,12 @@
+package com.example.dailynotes.repository;
+
+import com.example.dailynotes.entity.WeeklyPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WeeklyPlanRepository extends JpaRepository<WeeklyPlan,Long> {
+    List<WeeklyPlan> findByYearAndWeekNumber(int year,int weekNumber);
+}
