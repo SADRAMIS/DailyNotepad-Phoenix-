0e1fd511a27a743a55bfc836a0ef742bc6cd9ba2
Sadykov Ramis <ramis.sadykov.99@mail.ru>
2025-11-05 14:37:43 +0300

    add method:просмотр плана месяца

--- src/main/java/com/example/dailynotes/controller/MonthlyPlanController.java
+++ src/main/java/com/example/dailynotes/controller/MonthlyPlanController.java
@@ -1,10 +1,18 @@
 package com.example.dailynotes.controller;
 
+import com.example.dailynotes.entity.MonthlyPlan;
 import com.example.dailynotes.service.MonthlyPlanService;
 import com.example.dailynotes.service.TaskService;
 import com.example.dailynotes.service.TaskTemplateService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/plans/month")
@@ -17,5 +25,15 @@ public class MonthlyPlanController {
         this.monthlyPlanService = planService;
         this.taskService = taskService;
         this.taskTemplateService = templateService;
+    }
+
+    // Просмотр плана месяца
+    @GetMapping
+    public String showMonthlyPlan(@RequestParam int year, @RequestParam int month, Model model){
+        List<MonthlyPlan> plans = monthlyPlanService.getMonthlyPlans(year,month);
+        model.addAttribute("plans",plans);
+        int daysInMonth = LocalDate.of(year,month,1).lengthOfMonth();
+        model.addAttribute("days", IntStream.rangeClosed(1,daysInMonth).boxed().toList());
+        return "monthlyPlan";
     }
 }
